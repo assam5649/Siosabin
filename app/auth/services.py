f24519e52f5d38bcfe987cbc5f939adbb222a50d
assam5649 <assam5649.18@gmail.com>
2024-09-30 02:16:27 +0900

    add register and login

--- app/auth/services.py
+++ app/auth/services.py
@@ -1,16 +1,70 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from .models import User
-from . import db
+from .models import user
+import mysql.connector
 
 def register_service(data):
     hashed_password = generate_password_hash(data['password'], method='sha256')
-    new_user = User(username=data['username'], password_hash=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
-    return {'id': new_user.id, 'username': new_user.username}
+    # new_user = user(username=data['username'], password_hash=hashed_password)
+    config = mysql.connector.connect(
+        host='mysql-container',
+        port='3306',
+        user='root',
+        password='pass',
+        database='db'
+    )
+
+    config.ping(reconnect=True)
+    print(config.is_connected())
+
+    cur = config.cursor()
+    
+    insert_user_query =  """
+        INSERT INTO data
+        (name, password)
+        VALUES
+        (%s, %s);"""
+
+
+    cur.execute(insert_user_query, (data['username'], hashed_password))
+
+    config.commit()
+
+    get_name_query =  "SELECT * FROM users WHERE name = %s;"
+
+    cur.execute(get_name_query, (hashed_password))
+
+    
+    cur.statement
+    new_user_name = cur.fetchone()
+
+    cur.close()
+    config.close()
+
+    return {'username': new_user_name}
 
 def login_service(data):
-    user = User.query.filter_by(username=data['username']).first()
-    if user and check_password_hash(user.password_hash, data['password']):
+    config = mysql.connector.connect(
+        host='mysql-container',
+        port='3306',
+        user='root',
+        password='pass',
+        database='db'
+    )
+
+    config.ping(reconnect=True)
+    print(config.is_connected())
+
+    cur = config.cursor()
+
+    cur.execute("SELECT * FROM users WHERE name = %s", (data['name'],))
+    
+    cur.statement
+    password_hash = cur.fetchone()
+
+    cur.close()
+    config.close()
+    
+    if password_hash and check_password_hash(password_hash, data['password']):
         return {'message': 'Login successful'}
+    
     return {'message': 'Invalid credentials'}, 401
