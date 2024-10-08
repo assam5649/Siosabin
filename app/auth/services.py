714fdf546d4dba3e239c2eb8124693f7d02d5dbc
Shutaro Kawamitsu <assam5649.18@gmail.com>
2024-10-09 03:36:02 +0900

    add try and except. fix query

--- app/auth/services.py
+++ app/auth/services.py
@@ -1,65 +1,85 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 import mysql.connector
-from mysql.connector import IntegrityError
+from mysql.connector import IntegrityError, Error
 
 def register_service(data):
-    hashed_password = generate_password_hash(data['password'], method='pbkdf2:sha256')
-    config = mysql.connector.connect(
-        host='mysql-container',
-        port='3306',
-        user='root',
-        password='pass',
-        database='db'
-    )
-
-    config.ping(reconnect=True)
-    print(config.is_connected())
-
-    cur = config.cursor()
-    
-    insert_user_query =  """
-        INSERT INTO users
-        (name, password)
-        VALUES
-        (%s, %s)"""
     try:
+        hashed_password = generate_password_hash(data['password'], method='pbkdf2:sha256')
+        config = mysql.connector.connect(
+            host='mysql-container',
+            port='3306',
+            user='root',
+            password='pass',
+            database='db'
+        )
+
+        config.ping(reconnect=True)
+
+        cur = config.cursor()
+        
+        insert_user_query =  """
+            INSERT INTO users
+            (name, password)
+            VALUES
+            (%s, %s)"""
+        
         cur.execute(insert_user_query, (data['name'], hashed_password))
         config.commit()  # トランザクションをコミット
+
+        return {'message': 'Register successful'}, 200
+    
+    except Error as e:
+        print(f"Database error occurred: {e}")
+        return {'message': 'Database error occurred', 'error': str(e)}, 500
+    
     except IntegrityError as e:
-        return {'message': f"Error: {e}"}, 401  # エラーを表示
+        print(f"Integrity error occurred: {e}")
+        return {'message': 'User already exists'}, 409
 
-    cur.close()
-    config.close()
-
-    return {'message': 'Register successful'}    
+    finally:
+        if cur:
+            cur.close()
+        if config:
+            config.close()  
 
 def login_service(data):
-    config = mysql.connector.connect(
-        host='mysql-container',
-        port='3306',
-        user='root',
-        password='pass',
-        database='db'
-    )
+    try:
+        config = mysql.connector.connect(
+            host='mysql-container',
+            port='3306',
+            user='root',
+            password='pass',
+            database='db'
+        )
 
-    config.ping(reconnect=True)
-    print(config.is_connected())
+        config.ping(reconnect=True)
 
-    cur = config.cursor()
+        cur = config.cursor()
 
-    cur.execute("SELECT * FROM users WHERE name = %s", (data['name'],))
-    
-    cur.statement
-    user_record = cur.fetchone()
-    if user_record is None:
-        print(user_record)
+        cur.execute("SELECT * FROM users WHERE name = %s", (data['name'],))
+        
+        cur.statement
+        user_record = cur.fetchone()
+        if user_record is None:
+            return ({'message': 'user not found'}), 401
+        password_hash = user_record[2]
+
+        if password_hash and check_password_hash(password_hash, data['password']):
+            return {'message': 'Login successful'}, 200
+        
         return {'message': 'Invalid credentials'}, 401
-    password_hash = user_record[2]
+    
+    except Error as e:
+        print(f"Database error occurred: {e}")
+        return {'message': 'Database error occurred', 'error': str(e)}, 500
+    
+    except IntegrityError as e:
+        print(f"Integrity error occurred: {e}")
+        return {'message': 'Database error occurred', 'error': str(e)}, 409
 
-    cur.close()
-    config.close()
+    finally:
+        if cur:
+            cur.close()
+        if config:
+            config.close()
     
-    if password_hash and check_password_hash(password_hash, data['password']):
-        return {'message': 'Login successful'}
-    
-    return {'message': 'Invalid credentials'}, 401
--- app/main/services.py
+++ app/main/services.py
@@ -1,47 +1,76 @@
 import mysql.connector
+from mysql.connector import Error, IntegrityError
+
 
 def create_user_service(data):
-    config = mysql.connector.connect(
-        host='mysql-container',
-        port='3306',
-        user='root',
-        password='pass',
-        database='db'
-    )
+    try:
+        config = mysql.connector.connect(
+            host='mysql-container',
+            port='3306',
+            user='root',
+            password='pass',
+            database='db'
+        )
 
-    config.ping(reconnect=True)
-    print(config.is_connected())
+        config.ping(reconnect=True)
 
-    cur = config.cursor()
+        cur = config.cursor()
+
+        cur.execute("INSERT INTO data (device_id, location, in_tank, out_tank, salinity) VALUES (%s, %s, %s, %s, %s)", (data['device_id'], data['location'], data['in_tank'], data['out_tank'], data['salinity']))
+
+        config.commit()
+        
+        return data, 200
     
-    cur.execute("INSERT INTO users (name, password, location, remaining, salinity) VALUES (%s, %s, %s, %s, %s)", (data['name'], data['password'], data['location'], data['remaining'], data['salinity']))
+    except IntegrityError as e:
+        print(f"Integrity error occurred: {e}")
+        return {'message': 'Database error occurred'}, 409
 
-    config.commit()
+    except Error as e:
+        print(f"Error: {e}")
+        return {'message': 'Database error occurred', 'error': str(e)}, 500
 
-    cur.close()
-    config.close()
+    finally:
+        if cur:
+            cur.close()
+        if config:
+            config.close()
 
 def get_user_service(device_id):
-    config = mysql.connector.connect(
-        host='mysql-container',
-        port='3306',
-        user='root',
-        password='pass',
-        database='db'
-    )
+    try:
+        config = mysql.connector.connect(
+            host='mysql-container',
+            port='3306',
+            user='root',
+            password='pass',
+            database='db'
+        )
 
-    config.ping(reconnect=True)
-    print(config.is_connected())
+        config.ping(reconnect=True)
 
-    cur = config.cursor()
+        cur = config.cursor()
 
-    cur.execute("SELECT * FROM data WHERE device_id = %s ORDER BY id DESC LIMIT 1", (device_id,))
+        cur.execute("SELECT * FROM data WHERE device_id = %s ORDER BY id DESC LIMIT 1", (device_id,))
 
+        
+        cur.statement
+        result = cur.fetchone()
+        
+        if result is None:
+            return ({'message': 'device_id not found'}), 401
+
+        return result, 200
     
-    cur.statement
-    result = cur.fetchone()
-
-    cur.close()
-    config.close()
-
-    return result
+    except IntegrityError as e:
+        print(f"Integrity error occurred: {e}")
+        return {'message': 'User already exists'}, 409
+    
+    except Error as e:
+        print(f"Error: {e}")
+        return {'message': 'Database error occurred', 'error': str(e)}, 500
+        
+    finally:
+        if cur:
+            cur.close()
+        if config:
+            config.close()
