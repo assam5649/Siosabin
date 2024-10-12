887ef3b0a7eb67c82db9dc44cf057dad7492b849
assam5649 <assam5649.18@gmail.com>
2024-10-13 06:49:25 +0900

    add service

---
+++ app/predict/services.py
@@ -0,0 +1,45 @@
+import mysql.connector
+from mysql.connector import Error, IntegrityError
+import torch
+import numpy as np
+
+def insert_features(forecast):
+    try:
+        config = mysql.connector.connect(
+            host='mysql-container',
+            port='3306',
+            user='root',
+            password='pass',
+            database='db'
+        )
+
+        config.ping(reconnect=True)
+
+        cur = config.cursor()
+        
+        insert_features_query = """
+        INSERT INTO features
+        (year, month, hour, precipitation, tempMax, tempsMin)
+        VALUES
+        (%s, %s, %s, %s, %s, %s);"""
+
+        cur.execute(insert_features_query, (forecast['year'], forecast['month'], forecast['day'], forecast['hour'], forecast['precipitation'], forecast['tempMax'], forecast['tempMin']))
+
+        config.commit()
+    
+    except IntegrityError as e:
+        print(f"Integrity error occurred: {e}")
+        return {'message': 'Database error occurred'}
+
+    except Error as e:
+        print(f"Error: {e}")
+        return {'message': 'Database error occurred', 'error': str(e)}
+
+    finally:
+        if cur:
+            cur.close()
+        if config:
+            config.close()
+
+def save_target(target):
+    pass
