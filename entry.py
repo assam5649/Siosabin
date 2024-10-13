f43992b71ff765a50ca8aa6beb5cda44f917f799
assam5649 <assam5649.18@gmail.com>
2024-10-13 13:51:58 +0900

    complete entry

--- entry.py
+++ entry.py
@@ -1,10 +1,10 @@
 from app.predict.get import forecast
 from app.predict.services import insert_features
-from app.predict.utils import connect
+from app.predict.utils import connect, salinity
 from app.predict.predict import predict
 from app.predict.continual import update_model
 
 insert_features(forecast())
 predict(connect())
 
-# update_model()
+update_model(connect(), salinity())
