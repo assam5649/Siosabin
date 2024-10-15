d57d049804fddacef9f2365d41159e546d833b72
assam5649 <assam5649.18@gmail.com>
2024-10-15 16:35:07 +0900

    add TIMESTAMP

--- app/auth/models.py
+++ app/auth/models.py
@@ -20,6 +20,7 @@ def user():
             id INT PRIMARY KEY AUTO_INCREMENT,
             name VARCHAR(255) NOT NULL UNIQUE,
             password VARCHAR(255) NOT NULL,
+            share BOOLEAN DEFAULT TRUE,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
         )
