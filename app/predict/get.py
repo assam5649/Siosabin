598942281b18d12a514d4ee16372616c94bc05c9
assam5649 <assam5649.18@gmail.com>
2024-10-16 23:13:35 +0900

    add days forecast function

--- app/predict/get.py
+++ app/predict/get.py
@@ -3,12 +3,12 @@ import json
 import mysql.connector
 from mysql.connector import Error, IntegrityError
 from datetime import datetime
+import numpy as np
 
 def forecast():
     url = "https://www.jma.go.jp/bosai/forecast/data/forecast/471000.json"
     response = requests.get(url)
-    data = response.json()
-    print(type(data)) 
+    data = response.json() 
     report_datetime = data[0]["reportDatetime"]
 
     dt = datetime.fromisoformat(report_datetime)
@@ -65,3 +65,47 @@ def forecast():
     }
 
     return data
+
+def forecastDays():
+    url = "https://www.jma.go.jp/bosai/forecast/data/forecast/471000.json"
+    response = requests.get(url)
+    data = response.json()
+    time_series = []
+    weather_codes = []
+    temps_max = []
+    temps_min = []
+    rain_weather_codes = []
+
+    with open("./RainWeatherCode.txt", "r") as f:
+        for l in f:
+            rain_weather_codes.append(l.replace(", ", " ").split())
+
+    rain_weather_codes = np.array(rain_weather_codes)
+    rain_weather_codes = rain_weather_codes.flatten()
+
+    for i in range(len(data[1]["timeSeries"][0]["timeDefines"])):
+        time_series.append(data[1]["timeSeries"][0]["timeDefines"][i])
+        time_series[i] = datetime.fromisoformat(time_series[i])
+        weather_codes.append(data[1]["timeSeries"][0]["areas"][0]["weatherCodes"][i])
+        temps_max.append(data[1]["timeSeries"][1]["areas"][0]["tempsMax"][i])
+        temps_min.append(data[1]["timeSeries"][1]["areas"][0]["tempsMin"][i])
+
+    for i in range(len(data[1]["timeSeries"][0]["timeDefines"])):
+        if weather_codes[i] == rain_weather_codes[i]:
+            weather_codes[i] = 0
+        else:
+            weather_codes[i] = 1
+
+    result = []
+
+    for i in range(len(data[1]["timeSeries"][0]["timeDefines"]) - 1):
+        result.append({
+            "year": time_series[i+1].year,
+            "month": time_series[i+1].month,
+            "day": time_series[i+1].day,
+            "precipitation": weather_codes[i+1],
+            "tempMax": temps_max[i+1],
+            "tempMin": temps_min[i+1]
+        })
+
+    return result
