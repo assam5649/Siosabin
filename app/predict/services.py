fc3f2f53866408b48b5bee86f7f82026abb4c380
assam5649 <assam5649.18@gmail.com>
2024-10-16 23:16:31 +0900

    add save days target

--- app/predict/services.py
+++ app/predict/services.py
@@ -42,7 +42,46 @@ def insert_features(forecast):
         if config:
             config.close()
 
-def save_target(hour, target):
+def insert_featuresDays(forecast):
+    try:
+        config = mysql.connector.connect(
+            host='mysql-container',
+            port='3306',
+            user='root',
+            password='pass',
+            database='db'
+        )
+
+        config.ping(reconnect=True)
+
+        cur = config.cursor()
+        
+        insert_features_query = """
+        INSERT INTO featuresDays
+        (year, month, day, precipitation, tempMax, tempMin)
+        VALUES
+        (%s, %s, %s, %s, %s, %s);"""
+
+        for i in range(len(forecast)):
+           cur.execute(insert_features_query, (forecast[i]['year'], forecast[i]['month'], forecast[i]['day'], forecast[i]['precipitation'], forecast[i]['tempMax'], forecast[i]['tempMin']))
+
+        config.commit()
+    
+    except IntegrityError as e:
+        print(f"Integrity error occurred: {e}")
+        return {'message': 'Database error occurred'}
+
+    except Error as e:
+        print(f"Error: {e}")
+        return {'message': 'Database error occurred', 'error': str(e)}
+
+    finally:
+        if cur:
+            cur.close()
+        if config:
+            config.close()
+
+def saveTarget(hour, target):
     try:
         config = mysql.connector.connect(
             host='mysql-container',
@@ -58,9 +97,9 @@ def save_target(hour, target):
 
         insert_target_query = """
         INSERT INTO target
-        (future_offset, future_value)
+        (period_unit, future_offset, future_value)
         VALUES
-        (%s, %s);"""
+        ('hour', %s, %s);"""
 
         if hour == 5.0:
             hour = 17
@@ -68,12 +107,54 @@ def save_target(hour, target):
             hour = 5
         elif hour == 11.0:
             hour = 17
+        elif hour == 22.0:
+            hour = 5
         else:
             return {'message': 'hour is not five or fifteen'}
         
         category = categorize(target)
 
         cur.execute(insert_target_query, (hour, category))
+
+        config.commit()
+    
+    except IntegrityError as e:
+        print(f"Integrity error occurred: {e}")
+        return {'message': 'Database error occurred'}
+
+    except Error as e:
+        print(f"Error: {e}")
+        return {'message': 'Database error occurred', 'error': str(e)}
+
+    finally:
+        if cur:
+            cur.close()
+        if config:
+            config.close()
+
+def saveTargetDays(day, target):
+    try:
+        config = mysql.connector.connect(
+            host='mysql-container',
+            port='3306',
+            user='root',
+            password='pass',
+            database='db'
+        )
+
+        config.ping(reconnect=True)
+
+        cur = config.cursor()
+
+        insert_target_query = """
+        INSERT INTO target
+        (period_unit, future_offset, future_value)
+        VALUES
+        ('day', %s, %s);"""
+
+        category = categorize(target)
+
+        cur.execute(insert_target_query, (day, category))
 
         config.commit()
     
