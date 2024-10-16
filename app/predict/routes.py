5c6826cc54190b05b78e68b2364abb4eacbeddd7
Shutaro Kawamitsu <assam5649.18@gmail.com>
2024-10-17 00:12:35 +0900

    add day route and fix hour route

--- app/predict/routes.py
+++ app/predict/routes.py
@@ -1,12 +1,17 @@
 from flask import jsonify, request
 from . import predict
-from .services import salinity
+from .services import salinity, days_salinity
 
 @predict.route('/')
 def index():
     return jsonify({'message': 'Welcome to the predict Module...'})
     
-@predict.route('/salinity', methods=['GET'])
+@predict.route('/hour', methods=['GET'])
 def get_salinity():
     response, status_code = salinity()
+    return jsonify(response), status_code
+
+@predict.route('/day', methods=['GET'])
+def get_days_salinity():
+    response, status_code = days_salinity()
     return jsonify(response), status_code
